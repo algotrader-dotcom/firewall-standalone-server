e521b59268c4031fb42a8e177f4dec4a984fe7e3
Thuan Nguyen <ninhthuan82us@gmail.com>
2016-06-15 23:48:25 +0700

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # eth0 is public , eth1 is LAN with network 192.168.175.0/24
 # Flushing all rules
+$whitelist_ip = "your_ip"
+
 iptables -F
 iptables -X
 
@@ -18,5 +20,5 @@ iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 #iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 
 # Allow SSH from limited
-iptables -I INPUT -p tcp --dport 22 -s 123.123.123.123,192.168.175.0/24 -j ACCEPT
+iptables -I INPUT -p tcp --dport 22 -s $whitelist_ip,192.168.175.0/24 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j REJECT
